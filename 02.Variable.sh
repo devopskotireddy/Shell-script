0e2be00709dd3bc641b846e0627a5431c4ce13cc
devopskotireddy <devopskotireddy@gmail.com>
2024-01-07 12:42:14 +0530

    3,4 script

--- 02.Variable.sh
+++ 02.Variable.sh
@@ -3,6 +3,6 @@
    PERSON1=Ram
    PERSON2=koti
 
-    echo "$PERSON1" "Good morning"
-    echo "$PERSON2" " Very Good morning"
-    echo "$PERSON1" " How r you"
+    echo "$PERSON1:  Hello $PERSON2, Good morning"
+    echo "$PERSON2:  Very Good morning $PERSON1"
+    echo "$PERSON1:  How r you $PERSON2"
---
+++ 04.variable.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+
+   PERSON1=$1
+   PERSON2=$2
+
+    echo "$PERSON1: Good morning $PERSON2"
+    echo "$PERSON2: Very Good morning $PERSON1"
+    echo "$PERSON1:  How r you $PERSON2"
+    echo "$PERSON2: I am good $PERSON1"
